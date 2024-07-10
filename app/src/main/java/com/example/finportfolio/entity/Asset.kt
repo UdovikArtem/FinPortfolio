346020ff28c493e0b429aff08d9f6b626908ab1c
ShinzTer <kinadfeed@gmail.com>
2024-07-10 23:27:25 +0300

    fix

--- app/src/main/java/com/example/finportfolio/entity/Asset.kt
+++ app/src/main/java/com/example/finportfolio/entity/Asset.kt
@@ -2,20 +2,17 @@ package com.example.finportfolio.entity
 
 open class Asset(
     open val id: Int,
-    open val name: String,
-    open val price: Double
+    open val name: String
 )
 
 class Cash(
     override val id: Int,
     override val name: String,
-    override val price: Double,
     val currency: String
-) : Asset(id, name, price)
+) : Asset(id, name)
 
 data class Stock(
     override val id: Int,
     override val name: String,
-    override val price: Double,
     val ticker: String
-) : Asset(id, name, price)
+) : Asset(id, name)
--- app/src/main/java/com/example/finportfolio/repository/AssetRepository.kt
+++ app/src/main/java/com/example/finportfolio/repository/AssetRepository.kt
@@ -10,49 +10,41 @@ object AssetRepository {
             Cash(
                 id = 1,
                 name = "Belarusian Ruble",
-                price = 1.0,
                 currency = "BYN"
             ),
             Cash(
                 id = 2,
                 name = "Russian Ruble",
-                price = 0.036,
                 currency = "RUB"
             ),
             Cash(
                 id = 3,
                 name = "US Dollar",
-                price = 3.20,
                 currency = "USD"
             ),
             Cash(
                 id = 4,
                 name = "Euro",
-                price = 3.48,
                 currency = "EUR"
             ),
             Cash(
                 id = 5,
                 name = "Chinese Yuan",
-                price = 0.439,
                 currency = "CNY"
             ),
             Stock(
                 id = 6,
                 name = "Apple stock",
-                price = 731.8,
                 ticker = "AAPL"
             ),
             Stock(
                 id = 7,
                 name = "NVIDIA Corporation Stock",
-                price = 420.5,
                 ticker = "NVDA"
             ),
             Stock(
                 id = 8,
                 name = "Advanced Micro Devices, Inc. Stock",
-                price = 566.4,
                 ticker = "AMD"
             )
         ).associateBy { it.id }
